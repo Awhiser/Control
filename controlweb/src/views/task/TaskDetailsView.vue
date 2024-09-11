4ad7d2224a0cadeb6fde47fcff14355b441ca355
awhisper <ye123456>
2024-09-11 09:43:37 +0800

    feat taskDetails

---
+++ controlweb/src/views/task/TaskDetailsView.vue
@@ -0,0 +1,41 @@
+<template  >
+
+<a-layout>
+      <HeaderLayOut v-model:projectId="route.query.projectId" > </HeaderLayOut>
+      <a-layout-content :style="{padding:'15px'}" ><TaskDetails :taskId="route.query.taskId" > </TaskDetails></a-layout-content>
+</a-layout>
+
+
+   
+    
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+import { onMounted, watch } from 'vue'
+import TaskDetails from '@/components/task/TaskDetails.vue'
+
+import HeaderLayOut from '@/components/HeaderLayOut.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+
+const props = defineProps({
+    taskId: String
+})
+
+const selectedProject = ref(null);
+
+onMounted(() => {
+  //  console.log(route.params.taskId)
+})
+
+watch(() => route.params.projectId, (newId, oldId) => {
+
+})
+
+
+
+
+</script>
